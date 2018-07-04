12231a1330601e2848118d795328690f53a8807d
sun1ive <sunliveua@gmail.com>
2018-07-04 12:26:19 +0300

    created delete page

---
+++ app/common/containers/pages/MoviesEditPage/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { compose, withHandlers } from 'recompose';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router';
+import { provideHooks } from 'redial';
+import { translate } from 'react-i18next';
+import { updateMovie, fetchMovie } from '@/redux/data/movies';
+
+import MovieForm from '@/containers/forms/MovieForm';
+
+import withStyles from 'withStyles';
+import styles from './styles.scss';
+
+const MoviesCreatePage = ({ onSubmit, t, movie }) => (
+  <div className={styles.root}>
+    <div className={styles.title}>{t('Edit movie')}</div>
+    <div className={styles.back}>
+      <Link to="/movies">{t('Back to the list of movies')}</Link>
+    </div>
+    <div className={styles.form}>
+      <MovieForm movie={movie} onSubmit={onSubmit} />
+    </div>
+  </div>
+);
+
+export default compose(
+  withStyles(styles),
+  translate(),
+  withRouter,
+  provideHooks({
+    fetch: ({ dispatch, params, setProps }) =>
+      dispatch(fetchMovie(params.id)).then((response) => {
+        setProps({
+          movie: [...Object.values(response.payload.entities.movies)],
+        });
+      }),
+  }),
+  connect(
+    null,
+    {
+      updateMovie,
+    },
+  ),
+  withHandlers({
+    onSubmit: ({ updateMovie, router, params }) => async (formValues) => {
+      await updateMovie(params.id, formValues);
+      router.push('/movies');
+    },
+  }),
+)(MoviesCreatePage);
